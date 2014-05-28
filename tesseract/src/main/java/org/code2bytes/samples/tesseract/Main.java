a864c3551f19ce9b1901b3b2f91fbfee760f3880
Dieter Scholz <disc@code2bytes.org>
2014-05-28 15:04:14 +0200

    Get filename from system property
    Signed-off-by: Dieter Scholz <disc@code2bytes.org>

--- tesseract/src/main/java/org/code2bytes/samples/tesseract/Main.java
+++ tesseract/src/main/java/org/code2bytes/samples/tesseract/Main.java
@@ -10,8 +10,17 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			Tesseract tesseract = Tesseract.getInstance();
-			String result = tesseract.doOCR(new File("f:/DE000010338920A1-gd 21.12.05.pdf"));
-			System.out.println(result);
+			tesseract.setLanguage("deu");
+			String fileName = System.getProperty("c2b.tess4j.file");
+			if (fileName != null && !fileName.isEmpty()) {
+				File file = new File(fileName);
+				if (file.exists()) {
+					String result = tesseract.doOCR(file);
+					result = result.replaceAll("-\n", "");
+					result = result.replaceAll("\n", " ");
+					System.out.println(result);
+				}
+			}
 		} catch (TesseractException e) {
 			e.printStackTrace();
 		}
